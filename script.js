7a5aba705e32704b229faa229079a6489ce7079a
luccia1 <luccianjeri95@https://github.com/luccia1/>
2024-05-10 12:56:21 -0700

    added basic api functions

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+function changeTemp(response){
+  console.log(response);
+}
+
+
+function searchCity(city){
+    let apiKey="3d19633teeafa6c79049ab3o334f7b44";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} `;
+    axios.get(apiUrl) .then(changeTemp);
+}
+
+
 function handleSearch(event){
     event.preventDefault();
     let searchForm=document.querySelector("#search-form-input");
-    let townElement=document.querySelector("#city");
+
+    let =document.querySelector("#city");
     townElement.innerHTML=searchForm.value;
+    searchCity(searchForm.value);
     
     }
 
 
 let searchInput=document.querySelector("#search-form");
 searchInput.addEventListener("submit", handleSearch);
+
+
+

0c8882e72ff6c694b8c2c7387c3258f7d97b0ea6
luccia1 <luccianjeri95@https://github.com/luccia1/>
2024-05-12 22:43:29 -0700

    added some api code that changes humidity,windspeed,day and time and weather conditions

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 function changeTemp(response){
-  console.log(response);
+  
+  let temperatureElement=document.querySelector("#temperature");
+  let temperature=response.data.temperature.current;
+  let timeElement=document.querySelector("#time");
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windElement=document.querySelector("#wind-speed");
+  let iconElement=document.querySelector("#icon");
+  let date = new Date(response.data.time * 1000);
+
+
+
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML=Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 
@@ -14,7 +52,7 @@ function handleSearch(event){
     event.preventDefault();
     let searchForm=document.querySelector("#search-form-input");
 
-    let =document.querySelector("#city");
+    let townElement =document.querySelector("#city");
     townElement.innerHTML=searchForm.value;
     searchCity(searchForm.value);
     
